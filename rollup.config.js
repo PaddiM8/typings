daa24fad0341df87fda149c79d8070355bdb9d59
PaddiM8 <bakk@tuta.io>
2021-05-14 20:02:33 +0200

    Javascript minifying

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,27 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
 
 export default [{
     input: ['wwwroot/js/typeTest.js'],
     output: {
         file: 'wwwroot/dist/index.js',
         format: 'cjs'
-    }
+    },
+    plugins: [(process.env.NODE_ENV == "production" && uglify())]
 }, {
     input: 'wwwroot/js/common.js',
     output: {
         file: 'wwwroot/dist/common.js',
         format: 'cjs'
-    }
+    },
+    plugins: [(process.env.NODE_ENV == "production" && uglify())]
 }, {
     input: 'wwwroot/js/stats.js',
     output: {
         dir: 'wwwroot/dist',
         format: 'cjs'
     },
-    plugins: [nodeResolve(), commonjs()],
+    plugins: [nodeResolve(), commonjs(), (process.env.NODE_ENV == "production" && uglify())],
     preserveEntrySignatures: false
 }]
